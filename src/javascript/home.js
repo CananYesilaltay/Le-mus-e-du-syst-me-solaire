d81393d7a79f511084e1c046ddadfcd23ea72162
sibylleHrvt <sibylle.hervouet@hetic.net>
2020-03-27 01:13:41 +0100

    text correction + add home pop up in html

--- src/javascript/home.js
+++ src/javascript/home.js
@@ -1,35 +1,37 @@
+import * as THREE from 'three'
 import './style/home.styl'
 import imageSource from './images/astro-rocket.svg'
 
 
-const popUpBackground = document.createElement('div')
-popUpBackground.classList.add('pop-up-background')
-document.body.appendChild(popUpBackground)
+// const popUpBackground = document.createElement('div')
+// popUpBackground.classList.add('pop-up-background')
+// document.body.appendChild(popUpBackground)
 
-const popUpContainer = document.createElement('div')
-popUpContainer.classList.add('pop-up-container')
-popUpBackground.appendChild(popUpContainer)
+// const popUpContainer = document.createElement('div')
+// popUpContainer.classList.add('pop-up-container')
+// popUpBackground.appendChild(popUpContainer)
 
-const spatiumTitle = document.createElement('h1')
-popUpContainer.appendChild(spatiumTitle)
-spatiumTitle.textContent = 'SPATIUM'
+// const spatiumTitle = document.createElement('h1')
+// popUpContainer.appendChild(spatiumTitle)
+// spatiumTitle.textContent = 'SPATIUM'
 
-const spatiumText = document.createElement('p')
-popUpContainer.appendChild(spatiumText)
-spatiumText.textContent = 'Bienvenue dans Spatium, jeune spationaute ! Je suis là pour t’apprendre les merveilles de notre magnifique système solaire ! Prêt pour l’aventure ? '
+// const spatiumText = document.createElement('p')
+// popUpContainer.appendChild(spatiumText)
+// spatiumText.textContent = 'Bienvenue dans Spatium, jeune spationaute ! Je suis là pour t’apprendre les merveilles de notre magnifique système solaire ! C'est parti pour l'aventure !'
 
+const popUpContainer = document.querySelector('.pop-up-container')
 const astroRocket = new Image()
 astroRocket.src = imageSource
 popUpContainer.appendChild(astroRocket)
 
-const popUpButton = document.createElement('div')
-popUpButton.classList.add('pop-up-button')
-popUpContainer.appendChild(popUpButton)
+// const popUpButton = document.createElement('div')
+// popUpButton.classList.add('pop-up-button')
+// popUpContainer.appendChild(popUpButton)
 
-const popUpButtonContent = document.createElement('h3')
-popUpButtonContent.classList.add('pop-up-button-content')
-popUpButtonContent.textContent = 'Allons y !'
-popUpButton.appendChild(popUpButtonContent)
+// const popUpButtonContent = document.createElement('h3')
+// popUpButtonContent.classList.add('pop-up-button-content')
+// popUpButtonContent.textContent = 'Allons y !'
+// popUpButton.appendChild(popUpButtonContent)
 
 popUpButton.addEventListener('click', () =>
 {
